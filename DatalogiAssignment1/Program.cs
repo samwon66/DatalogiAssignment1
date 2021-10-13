bd8065f7d33b610743f8e8177ee14458eba552d9
Sammy Wong <66samwon@gafe.molndal.se>
2021-10-13 15:25:49 +0200

    Add menu

--- DatalogiAssignment1/Program.cs
+++ DatalogiAssignment1/Program.cs
@@ -6,7 +6,7 @@ namespace DatalogiAssignment1
     {
         static void Main(string[] args)
         {
-            
+            Menu menu = new Menu();
         }
     }
 }
--- DatalogiAssignment1/Utilities/Utils.cs
+++ DatalogiAssignment1/Utilities/Utils.cs
@@ -9,12 +9,24 @@ namespace DatalogiAssignment1.Utilities
     public static class Utils
     {
         /// <summary>
-        /// Kolla om ett heltal är primtal
+        /// Kolla om ett heltal är primtal.
+        /// Om talet är 1 eller mindre, så är det inget primtal och 2 är ett primtal.
+        /// Sen kollar man från 2 t.o.m. det talet som är kvadraten av heltalet som angett.
         /// </summary>
-        /// <param name="nr"></param>
+        /// <param name="nr">ett heltal som ska kollas.</param>
         /// <returns>true om det är primtal, annars false.</returns>
         public static bool PrimeCheck(int nr)
         {
+            if (nr <= 1) return false;
+            if (nr == 2) return true;
+            var stop = Math.Round(Math.Sqrt(nr));
+            for (int i = 2; i <= stop; i++)
+            {
+                if (nr % i == 0)
+                {
+                    return false;
+                }
+            }
             return true;
         }
 
