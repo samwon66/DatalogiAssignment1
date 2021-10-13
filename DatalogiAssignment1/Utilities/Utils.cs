192527b1f2b978601367bf929d4fb6996374959d
Sammy Wong <66samwon@gafe.molndal.se>
2021-10-13 14:12:39 +0200

    Add project files.

---
+++ DatalogiAssignment1/Utilities/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatalogiAssignment1.Utilities
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Kolla om ett heltal är primtal
+        /// </summary>
+        /// <param name="nr"></param>
+        /// <returns>true om det är primtal, annars false.</returns>
+        public static bool PrimeCheck(int nr)
+        {
+            return true;
+        }
+
+
+    }
+}
